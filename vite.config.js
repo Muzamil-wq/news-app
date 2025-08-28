390afcda370e9b7a3a7811fa449c7d222167ca94
Your Name <cd..>
2025-08-28 16:54:08 +0500

    Migrate App.js/index.js to App.jsx/index.jsx and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/news-app/',   // 👈 important for GitHub Pages or subfolder
+  base: '/news-app/',   // repo name
 })
